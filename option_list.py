b9f60ea3f46aff6e886fb88d7dea55231a3542a3
wenyen-hsu <vm3jp6u04@gmail.com>
2024-04-12 21:47:50 +0800

    v2: add delete and update description

--- option_list.py
+++ option_list.py
@@ -48,33 +48,62 @@ def search_command(conn, command_name):
     except sqlite3.Error as e:
         print(f"Error searching command: {e}")
 
+def delete_command(conn, command_name):
+    """删除指定的命令"""
+    sql = "DELETE FROM commands WHERE name=?"
+    cursor = conn.cursor()
+    cursor.execute(sql, (command_name,))
+    conn.commit()
+    if cursor.rowcount > 0:
+        print(f"命令 '{command_name}' 已被删除。")
+    else:
+        print("没有找到该命令。")
+
+def update_description(conn, command_name, new_description):
+    """更新命令的描述"""
+    sql = "UPDATE commands SET description=? WHERE name=?"
+    cursor = conn.cursor()
+    cursor.execute(sql, (new_description, command_name))
+    conn.commit()
+    if cursor.rowcount > 0:
+        print(f"命令 '{command_name}' 的描述已更新。")
+    else:
+        print("没有找到该命令。")
+
 def main():
     database = "commands.db"
-
-    # 創建數據庫連接並創建表格
     conn = create_connection(database)
     if conn is not None:
         create_table(conn)
 
         while True:
-            print("\n主菜單:")
+            print("\n主菜单:")
             print("1. 新增 Command")
             print("2. 搜索 Command")
-            print("3. 退出")
-            choice = input("請選擇一個選項 (1/2/3): ")
+            print("3. 删除 Command")
+            print("4. 更新 Command 描述")
+            print("5. 退出")
+            choice = input("请选择一个选项 (1/2/3/4/5): ")
 
             if choice == '1':
-                command_name = input("請輸入 Command 名稱: ")
-                description = input("請輸入 Command 註解: ")
+                command_name = input("请输入 Command 名称: ")
+                description = input("请输入 Command 描述: ")
                 add_command(conn, command_name, description)
             elif choice == '2':
-                command_name = input("請輸入要搜索的 Command 名稱: ")
+                command_name = input("请输入要搜索的 Command 名称: ")
                 search_command(conn, command_name)
             elif choice == '3':
-                print("退出程式。")
+                command_name = input("请输入要删除的 Command 名称: ")
+                delete_command(conn, command_name)
+            elif choice == '4':
+                command_name = input("请输入要更新描述的 Command 名称: ")
+                new_description = input("请输入新的描述: ")
+                update_description(conn, command_name, new_description)
+            elif choice == '5':
+                print("退出程序。")
                 break
             else:
-                print("無效的選項，請重新輸入！")
+                print("无效的选项，请重新输入！")
 
         conn.close()
 
